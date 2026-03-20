bd0468c9f49aedf0f11e21294504645272805c91
ankitrout07 <ankitanupam3@gmail.com>
2026-03-20 23:03:03 +0530

    Update App01

--- networking/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update -y
-apt-get install -y apache2
-systemctl start apache2
-systemctl enable apache2
-echo "<h1>Hello from Fortress-VNet Tier 2 (Azure)</h1>" > /var/www/html/index.html
